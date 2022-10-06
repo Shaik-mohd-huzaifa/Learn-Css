0afb00857f577d10329baf51ed6f5e6a067e1e98
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2022-10-06 15:11:08 +0530

    Comment to divide the sections

--- Flex Box/main.js
+++ Flex Box/main.js
@@ -26,8 +26,7 @@ function removeactiveClasses() {
   });
 }
 
-// Event Listener for FD
-
+// Event Listener for Flex Direction
 document.querySelectorAll("#FD").forEach((FD) => {
   FD.addEventListener("click", function (FD) {
     // Here we get the id of the Clicked element
@@ -57,38 +56,4 @@ document.querySelectorAll("#FD").forEach((FD) => {
   });
 });
 
-// // for the Justify-content:  List items
-// const toggleButtonsforDirection = document.querySelectorAll(".toggleButtonD");
-
-// toggleButtonsforDiretion.forEach((toggleButtonD) => {
-//   toggleButtonD.addEventListener("click", (e) => {
-//     const targettedText = document.getElementById(id);
-//     targettedText.classList.add("active-content");
-
-//     removeactiveClasses();
-//     toggleButtonD.classList.add("active");
-//   });
-// });
-// function removeactiveClasses() {
-//   toggleButtonsforDiretion.forEach((toggleButtonD) => {
-//     toggleButtonD.classList.remove("active");
-//   });
-// }
-
-// // for the Justify Content List items
-// const toggleButtonsforJustifyContent =
-//   document.querySelectorAll(".toggleButtonJ");
-
-// toggleButtonsforJustifyContent.forEach((toggleButtonJ) => {
-//   toggleButtonJ.addEventListener("click", () => {
-//     const targettedText = document.getElementById(id);
-//     targettedText.classList.add("active-content");
-//     removeactiveClasses();
-//     toggleButtonJ.classList.add("active");
-//   });
-// });
-// function removeactiveClasses() {
-//   toggleButtonsforJustifyContent.forEach((toggleButtonJ) => {
-//     toggleButtonJ.classList.remove("active");
-//   });
-// }
+// Event Listener for Justify Content
